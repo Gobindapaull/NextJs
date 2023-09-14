5baeb8c67c630fb679873acd2f8f9c78f4f82aab
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-09-14 13:39:00 +0530

    Create news-api.js

---
+++ axios/news-api.js
@@ -0,0 +1,36 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+const inter = Inter({ subsets: ['latin'] })
+
+import axios from 'axios'
+import { useState } from 'react'
+
+export default function Home() {
+
+  const [data, setData] = useState()
+
+  const api = "dd3590bc9b1e46659007d65a915febc3"
+  let tesla = "tesla"
+  let date = '2023-05-17'
+  let sortBy = 'publishedAt'
+  const url = `https://newsapi.org/v2/everything?q=${tesla}&from=${date}$sortBy=${sortBy}&apiKey=${api}`
+
+  const GrabNews = () => {
+    axios.get(url)
+    .then((response) => {
+      console.clear()
+      setData(response.data)
+      console.log(response.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  return (
+    <>
+      <main className={`${styles.main} ${inter.className}`}>
+       <button onClick={() => GrabNews()}>Grab Info</button>
+      </main>
+    </>
+  )
+}

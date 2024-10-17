a049203d5c5015ff5a67a11ba6ec74c0ed2a2ae5
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-10-17 23:15:33 +0530

    Create page.tsx

---
+++ NAVBAR/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar/page";
+
+export default function Home() {
+  return <div>
+   <Navbar />
+   <div className="text-8xl text-center py-80">
+    HOME PAGE
+   </div>
+  </div>;
+}

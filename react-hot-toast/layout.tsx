0c9a2bf4021a8a968c8e6586c16c6f5cb1d5ec3e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-21 00:28:19 +0530

    Create layout.tsx

---
+++ react-hot-toast/layout.tsx
@@ -0,0 +1,10 @@
+<Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 5000,
+            style: {
+              background: "#000000",
+              color: "#fff",
+            },
+          }}
+        />

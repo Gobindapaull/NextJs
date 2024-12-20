fc51d2cf42d20be00a40170bb8689ec8d57a990b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-21 00:15:32 +0530

    Create page.tsx

---
+++ react-hot-toast/page.tsx
@@ -0,0 +1,10 @@
+import Demo from "@/components/Demo";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <p>Our Next Application</p>
+      <Demo />
+    </div>
+  );
+}

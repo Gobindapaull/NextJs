54924e0fa3422a06d0282dd34c1cfa4e9e5c90ae
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-21 01:02:51 +0530

    Update page.tsx

--- react-hot-toast/page.tsx
+++ react-hot-toast/page.tsx
@@ -2,7 +2,7 @@ import Demo from "@/components/Demo";
 
 export default function Home() {
   return (
-    <div className="p-10">
+    <div className="p-10 bg-green-400">
       <p>Our Next Application</p>
       <Demo />
     </div>

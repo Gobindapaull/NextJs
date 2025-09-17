a99ddaf52a4dbcafa4b75ed3eb45f58687408c50
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-18 01:59:16 +0530

    Create page.tsx

---
+++ app/darkmode/appclerk/page.tsx
@@ -0,0 +1,11 @@
+import { SignOutButton, SignUpButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div className="w-full bg-black text-center text-white p-3">
+      <SignUpButton />
+      <SignOutButton /> 
+    </div>
+  );
+}

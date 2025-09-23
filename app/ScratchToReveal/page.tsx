b17c115b6fce91fb5b2e5499e6dab888b843b248
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-23 08:13:13 +0530

    Create page.tsx

---
+++ app/ScratchToReveal/page.tsx
@@ -0,0 +1,10 @@
+import { ScratchToRevealDemo } from "@/components/ScratchToRevealDemo";
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <ScratchToRevealDemo />
+    </div>
+  );
+}
